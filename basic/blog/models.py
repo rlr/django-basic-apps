6ab432522c64c77c1c3ffefe811358da636d3f6b
Ricky Rosario <rickyrosario@gmail.com>
2009-12-01 01:26:35 -0500

    need slug to be up do 200 chars too

--- basic/blog/models.py
+++ basic/blog/models.py
@@ -46,7 +46,7 @@ class Post(models.Model):
         (2, _('Public')),
     )
     title           = models.CharField(_('title'), max_length=200)
-    slug            = models.SlugField(_('slug'), unique_for_date='publish')
+    slug            = models.SlugField(_('slug'), max_length=200, unique_for_date='publish')
     author          = models.ForeignKey(User, blank=True, null=True)
 
     markup          = MarkupField(default='markdown')
