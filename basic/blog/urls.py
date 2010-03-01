65d205a811e1831b8a1e06ab5de4d991a1b7e253
Ricky Rosario <rickyrosario@gmail.com>
2010-03-01 11:59:41 -0500

    reordered url patterns due to blog/<slug>s greediness

--- basic/blog/urls.py
+++ basic/blog/urls.py
@@ -3,6 +3,13 @@ from basic.blog import views as blog_views
 
 
 urlpatterns = patterns('',
+    url(r'^categories/$',
+        view=blog_views.category_list,
+        name='blog_category_list'),
+    url(r'^(?P<year>\d{4})/$',
+        view=blog_views.post_archive_year,
+        name='blog_archive_year'),
+    
     url(r'^(?P<slug>[-\w]+)/$',
         view=blog_views.post_detail,
         name='blog_detail'),
@@ -14,24 +21,17 @@ urlpatterns = patterns('',
     url(r'^(?P<year>\d{4})/(?P<month>\w{3})/$',
         view=blog_views.post_archive_month,
         name='blog_archive_month'),
-
-    url(r'^(?P<year>\d{4})/$',
-        view=blog_views.post_archive_year,
-        name='blog_archive_year'),
+    
 
     url(r'^categories/(?P<slug>[-\w]+)/$',
         view=blog_views.category_detail,
         name='blog_category_detail'),
 
-    url (r'^categories/$',
-        view=blog_views.category_list,
-        name='blog_category_list'),
-
     url(r'^tags/(?P<slug>[-\w]+)/$',
         view=blog_views.tag_detail,
         name='blog_tag_detail'),
 
-    url (r'^search/$',
+    url(r'^search/$',
         view=blog_views.search,
         name='blog_search'),
 
